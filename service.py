35d07a63990696b29ac3c0b3b014aa7812999dcd
Camilo <camilor67@gmail.com>
2017-06-06 00:27:25 -0500

    lectura de archivos mensuales

--- service.py
+++ service.py
@@ -110,9 +110,10 @@ def service():
 		wavg = True
 	else:
 		wavg = False
-		
+
 	if request.query.climatology == "true":
 		wclm = True
+		monthlyYear = 0
 	else:
 		wclm = False
 
@@ -125,98 +126,130 @@ def service():
 	fileName = request.query.index.lower()+"_bcsd_"+request.query.scenario.lower()+"_"+wgcm
 	folderModels = "/mnt/data_climatewizard/AR5_Global_Daily_25k/out_stats_tiff/"
 	folder = folderModels+wgcm+"/"
+	output_item = ""
 	if wclm:
 		folder = folder + "split/"
-	
+
 	allfiles = find(fileName+"*", folder)
 
-	if allfiles:
-		name = allfiles.split(folder)
-		acroIndex = name[1].split("_")
-		period = acroIndex[4].split("-")
-		startDate = int(acroIndex[4].split("-")[0])
+	if len(allfiles) > 0:
 		json_output = {'name' : index[request.query.index.lower()], 'acronym':request.query.index,'model':wgcm,'scenario':request.query.scenario.lower() ,'values':[]}
-		ds = gdal.Open(folder+name[1], GA_ReadOnly)
-		if ds is None:
-				print 'Failed open file'
-				sys.exit(1)
-		bands = ds.RasterCount
+		for nameFile in allfiles:
+			name = nameFile.split(folder)
+			acroIndex = name[1].split("_")
+			period = acroIndex[4].split("-")
+			startDate = int(period[0])
 
-		if request.query.geojson:
-			file = '/tmp/cwizard'+time.strftime("%H:%M:%S")+'.geojson'
-			with open(file, 'w') as file_:
-				file_.write(request.query.geojson)
+			ds = gdal.Open(folder+name[1], GA_ReadOnly)
+			if ds is None:
+					print 'Failed open file'
+					sys.exit(1)
+			bands = ds.RasterCount
 
-			if request.query.stats:
-				tstats = request.query.stats.split(",")
-				rstats = []
-				for stat in tstats:
-					rstats.append(stat)
-			else :
-				rstats = ['min', 'max', 'median', 'mean','percentile_5','percentile_25','percentile_75','percentile_95']
+			if request.query.geojson:
+				file = '/tmp/cwizard'+time.strftime("%H:%M:%S")+'.geojson'
+				with open(file, 'w') as file_:
+					file_.write(request.query.geojson)
 
-			for band in range( bands ):
-				band += 1
-				if int(wrange[0]) <= int(band+startDate-1) <= int(wrange[1]):
-					with fiona.open(file) as src:
-						zs = zonal_stats(src, folder+name[1], band=band,stats=rstats)
+				if request.query.stats:
+					tstats = request.query.stats.split(",")
+					rstats = []
+					for stat in tstats:
+						rstats.append(stat)
+				else :
+					rstats = ['min', 'max', 'median', 'mean', 'std','percentile_5','percentile_25','percentile_75','percentile_95']
 
-					output_item = {'date' : int(band+startDate-1) , 'value' : zs[0]}
-					json_output['values'].append(output_item)
-			return json_output
-		else:
-			baselineAvg = 0
-			if request.query.baseline:
-				wbaseline = request.query.baseline.split("-")
-				if (int(wbaseline[1]) - int(wbaseline[0])) < 19 :
-					return {"error":"Baseline range must be equal or greater than 20 years"}
-				elif request.query.scenario.lower() == "historical":
-					return {"error":"Scenario must be different to historical"}
-				else:
-					baseFileName = request.query.index.lower()+"_bcsd_historical_"+wgcm
-					baseFile = find(baseFileName+"*", folder)
-					baselineAvg = calcAvg (baseFile, folder, wbaseline, float(request.query.lat), float(request.query.lon))
-			lat = float(request.query.lat)
-			lon = float(request.query.lon)
-			
-			transf = ds.GetGeoTransform()
-
-			px = (lon-transf[0])/transf[1]
-			py = (lat-transf[3])/transf[5]
-
-			if wrange and wrange != "false":
-				avg = 0
 				for band in range( bands ):
 					band += 1
-					if int(wrange[0]) <= int(band+startDate-1) <= int(wrange[1]):
+
+					if wclm:
+						if int(wrange[0]) <= int(monthlyYear+startDate) <= int(wrange[1]):
+							with fiona.open(file) as src:
+								zs = zonal_stats(src, folder+name[1], band=band,stats=rstats)
+							json_output['values'].append({'date' : str(int(monthlyYear+startDate))+"-"+str(int(((band-1)%12)+1)) , 'value' : zs[0]})
+							if (band % 12) == 0:
+								monthlyYear += 1
+					else:
+						if int(wrange[0]) <= int(band+startDate-1) <= int(wrange[1]):
+							with fiona.open(file) as src:
+								zs = zonal_stats(src, folder+name[1], band=band,stats=rstats)
+							json_output['values'].append({'date' : int(band+startDate-1) , 'value' : zs[0]})
+
+					# json_output['values'].append(output_item)
+				# return json_output
+			else:
+				baselineAvg = 0
+				if request.query.baseline:
+					wbaseline = request.query.baseline.split("-")
+					if (int(wbaseline[1]) - int(wbaseline[0])) < 19 :
+						return {"error":"Baseline range must be equal or greater than 20 years"}
+					elif request.query.scenario.lower() == "historical":
+						return {"error":"Scenario must be different to historical"}
+					else:
+						baseFileName = request.query.index.lower()+"_bcsd_historical_"+wgcm
+						baseFile = find(baseFileName+"*", folder)
+						baselineAvg = calcAvg (baseFile, folder, wbaseline, float(request.query.lat), float(request.query.lon))
+				lat = float(request.query.lat)
+				lon = float(request.query.lon)
+				
+				transf = ds.GetGeoTransform()
+
+				px = (lon-transf[0])/transf[1]
+				py = (lat-transf[3])/transf[5]
+
+				if wrange and wrange != "false":
+					avg = 0
+					for band in range( bands ):
+						band += 1
+						if wclm:
+							if int(wrange[0]) <= int(monthlyYear+startDate) <= int(wrange[1]):
+								srcband = ds.GetRasterBand(band)
+								structval = srcband.ReadRaster(int(px), int(py), 1, 1, buf_type=srcband.DataType )
+								bandtype = gdal.GetDataTypeName(srcband.DataType)
+								intval = struct.unpack(fmttypes[bandtype] , structval)
+								if wavg:
+									avg += (round((float(intval[0])/100),2))+factor
+								else:
+									output_item = {'date' : str(int(monthlyYear+startDate))+"-"+str(int(((band-1)%12)+1)) , 'value' : ((round((float(intval[0])/100),2))+factor)-baselineAvg}
+									if (band % 12) == 0:
+										monthlyYear += 1
+									json_output['values'].append(output_item)
+						else:
+							if int(wrange[0]) <= int(band+startDate-1) <= int(wrange[1]):
+								srcband = ds.GetRasterBand(band)
+								structval = srcband.ReadRaster(int(px), int(py), 1, 1, buf_type=srcband.DataType )
+								bandtype = gdal.GetDataTypeName(srcband.DataType)
+								intval = struct.unpack(fmttypes[bandtype] , structval)
+								if wavg:
+									avg += (round((float(intval[0])/100),2))+factor
+								else:
+									output_item = {'date' : int(band+startDate-1) , 'value' : ((round((float(intval[0])/100),2))+factor)-baselineAvg}
+									json_output['values'].append(output_item)
+					if avg != 0 and wavg:
+						avg = (avg / (int(wrange[1]) - int(wrange[0]) + 1)) - baselineAvg +factor
+						output_item = {'date' : 'avg_'+wrange[0]+"-"+wrange[1] , 'value' : str(avg)}
+						json_output['values'].append(output_item)
+					elif avg == 0 and wavg:
+						output_item = {'date' : 'avg_'+wrange[0]+"-"+wrange[1] , 'value' : 'out of range'}
+						json_output['values'].append(output_item)
+					# return json_output
+
+				else:
+					for band in range( bands ):
+						band += 1
 						srcband = ds.GetRasterBand(band)
 						structval = srcband.ReadRaster(int(px), int(py), 1, 1, buf_type=srcband.DataType )
 						bandtype = gdal.GetDataTypeName(srcband.DataType)
 						intval = struct.unpack(fmttypes[bandtype] , structval)
-						if wavg:
-							avg += (round((float(intval[0])/100),2))+factor
-						else:
+						if wclm:
+							output_item = {'date' : str(int(monthlyYear+startDate))+"-"+str(int(((band-1)%12)+1)) , 'value' : ((round((float(intval[0])/100),2))+factor)-baselineAvg}
+							if (band % 12) == 0:
+								monthlyYear += 1
+						else: 
 							output_item = {'date' : int(band+startDate-1) , 'value' : ((round((float(intval[0])/100),2))+factor)-baselineAvg}
-							json_output['values'].append(output_item)
-				if avg != 0 and wavg:
-					avg = (avg / (int(wrange[1]) - int(wrange[0]) + 1)) - baselineAvg +factor
-					output_item = {'date' : 'avg_'+wrange[0]+"-"+wrange[1] , 'value' : str(avg)}
-					json_output['values'].append(output_item)
-				elif avg == 0 and wavg:
-					output_item = {'date' : 'avg_'+wrange[0]+"-"+wrange[1] , 'value' : 'out of range'}
-					json_output['values'].append(output_item)
-				return json_output
-
-			else:
-				for band in range( bands ):
-					band += 1
-					srcband = ds.GetRasterBand(band)
-					structval = srcband.ReadRaster(int(px), int(py), 1, 1, buf_type=srcband.DataType )
-					bandtype = gdal.GetDataTypeName(srcband.DataType)
-					intval = struct.unpack(fmttypes[bandtype] , structval)
-					output_item = {'date' : int(band+startDate-1) , 'value' : ((round((float(intval[0])/100),2))+factor)-baselineAvg}
-					json_output['values'].append(output_item)
-				return json_output
+						json_output['values'].append(output_item)
+					# return json_output
+		return json_output	
 	else :
 		return {"error":"Data not found"}
 
@@ -241,112 +274,145 @@ def do_service():
 		wavg = True
 	else:
 		wavg = False
-		
+
 	if request.query.climatology == "true":
 		wclm = True
+		monthlyYear = 0
 	else:
 		wclm = False
 
 	# for convert to celsius
-	if request.query.index.lower()=="TXAVG" or request.query.index.lower()=="TNAVG" or request.query.index.lower()=="TXX" or request.query.index.lower()=="TNN":
+	if request.query.index.lower()=="txavg" or request.query.index.lower()=="tnavg" or request.query.index.lower()=="txx" or request.query.index.lower()=="tnn":
 		factor=-273.15 
 	else:
 		factor=0
 		
-	fileName = request.query.index+"_BCSD_"+request.query.scenario.lower()+"_"+wgcm
+	fileName = request.query.index.lower()+"_bcsd_"+request.query.scenario.lower()+"_"+wgcm
 	folderModels = "/mnt/data_climatewizard/AR5_Global_Daily_25k/out_stats_tiff/"
 	folder = folderModels+wgcm+"/"
+	output_item = ""
 	if wclm:
 		folder = folder + "split/"
-		
+
 	allfiles = find(fileName+"*", folder)
 
-	if allfiles:
-		name = allfiles.split(folder)
-		acroIndex = name[1].split("_")
-		period = acroIndex[4].split("-")
-		startDate = int(acroIndex[4].split("-")[0])
+	if len(allfiles) > 0:
 		json_output = {'name' : index[request.query.index.lower()], 'acronym':request.query.index,'model':wgcm,'scenario':request.query.scenario.lower() ,'values':[]}
-		ds = gdal.Open(folder+name[1], GA_ReadOnly)
-		if ds is None:
-				print 'Failed open file'
-				sys.exit(1)
-		bands = ds.RasterCount
+		for nameFile in allfiles:
+			name = nameFile.split(folder)
+			acroIndex = name[1].split("_")
+			period = acroIndex[4].split("-")
+			startDate = int(period[0])
 
-		if request.query.geojson:
-			file = '/tmp/cwizard'+time.strftime("%H:%M:%S")+'.geojson'
-			with open(file, 'w') as file_:
-				file_.write(request.query.geojson)
+			ds = gdal.Open(folder+name[1], GA_ReadOnly)
+			if ds is None:
+					print 'Failed open file'
+					sys.exit(1)
+			bands = ds.RasterCount
 
-			if request.query.stats:
-				tstats = request.query.stats.split(",")
-				rstats = []
-				for stat in tstats:
-					rstats.append(stat)
-			else :
-				rstats = ['min', 'max', 'median', 'mean','percentile_5','percentile_25','percentile_75','percentile_95']
+			if request.query.geojson:
+				file = '/tmp/cwizard'+time.strftime("%H:%M:%S")+'.geojson'
+				with open(file, 'w') as file_:
+					file_.write(request.query.geojson)
 
-			for band in range( bands ):
-				band += 1
-				if int(wrange[0]) <= int(band+startDate-1) <= int(wrange[1]):
-					with fiona.open(file) as src:
-						zs = zonal_stats(src, folder+name[1], band=band,stats=rstats)
+				if request.query.stats:
+					tstats = request.query.stats.split(",")
+					rstats = []
+					for stat in tstats:
+						rstats.append(stat)
+				else :
+					rstats = ['min', 'max', 'median', 'mean', 'std','percentile_5','percentile_25','percentile_75','percentile_95']
 
-					output_item = {'date' : int(band+startDate-1) , 'value' : zs[0]}
-					json_output['values'].append(output_item)
-			return json_output
-		else:
-			baselineAvg = 0
-			if request.query.baseline:
-				wbaseline = request.query.baseline.split("-")
-				if (int(wbaseline[1]) - int(wbaseline[0])) < 19 :
-					return {"error":"Baseline range must be equal or greater than 20 years"}
-				elif request.query.scenario.lower() == "historical":
-					return {"error":"Scenario must be different to historical"}
-				else:
-					baseFileName = request.query.index.lower()+"_bcsd_historical_"+wgcm
-					baseFile = find(baseFileName+"*", folder)
-					baselineAvg = calcAvg (baseFile, folder, wbaseline, float(request.query.lat), float(request.query.lon))
-					lat = float(request.query.lat)
-					lon = float(request.query.lon)
-					
-					transf = ds.GetGeoTransform()
-
-			px = (lon-transf[0])/transf[1]
-			py = (lat-transf[3])/transf[5]
-
-			if wrange and wrange != "false":
-				avg = 0
 				for band in range( bands ):
 					band += 1
-					if int(wrange[0]) <= int(band+startDate-1) <= int(wrange[1]):
+
+					if wclm:
+						if int(wrange[0]) <= int(monthlyYear+startDate) <= int(wrange[1]):
+							with fiona.open(file) as src:
+								zs = zonal_stats(src, folder+name[1], band=band,stats=rstats)
+							json_output['values'].append({'date' : str(int(monthlyYear+startDate))+"-"+str(int(((band-1)%12)+1)) , 'value' : zs[0]})
+							if (band % 12) == 0:
+								monthlyYear += 1
+					else:
+						if int(wrange[0]) <= int(band+startDate-1) <= int(wrange[1]):
+							with fiona.open(file) as src:
+								zs = zonal_stats(src, folder+name[1], band=band,stats=rstats)
+							json_output['values'].append({'date' : int(band+startDate-1) , 'value' : zs[0]})
+
+					# json_output['values'].append(output_item)
+				# return json_output
+			else:
+				baselineAvg = 0
+				if request.query.baseline:
+					wbaseline = request.query.baseline.split("-")
+					if (int(wbaseline[1]) - int(wbaseline[0])) < 19 :
+						return {"error":"Baseline range must be equal or greater than 20 years"}
+					elif request.query.scenario.lower() == "historical":
+						return {"error":"Scenario must be different to historical"}
+					else:
+						baseFileName = request.query.index.lower()+"_bcsd_historical_"+wgcm
+						baseFile = find(baseFileName+"*", folder)
+						baselineAvg = calcAvg (baseFile, folder, wbaseline, float(request.query.lat), float(request.query.lon))
+				lat = float(request.query.lat)
+				lon = float(request.query.lon)
+				
+				transf = ds.GetGeoTransform()
+
+				px = (lon-transf[0])/transf[1]
+				py = (lat-transf[3])/transf[5]
+
+				if wrange and wrange != "false":
+					avg = 0
+					for band in range( bands ):
+						band += 1
+						if wclm:
+							if int(wrange[0]) <= int(monthlyYear+startDate) <= int(wrange[1]):
+								srcband = ds.GetRasterBand(band)
+								structval = srcband.ReadRaster(int(px), int(py), 1, 1, buf_type=srcband.DataType )
+								bandtype = gdal.GetDataTypeName(srcband.DataType)
+								intval = struct.unpack(fmttypes[bandtype] , structval)
+								if wavg:
+									avg += (round((float(intval[0])/100),2))+factor
+								else:
+									output_item = {'date' : str(int(monthlyYear+startDate))+"-"+str(int(((band-1)%12)+1)) , 'value' : ((round((float(intval[0])/100),2))+factor)-baselineAvg}
+									if (band % 12) == 0:
+										monthlyYear += 1
+									json_output['values'].append(output_item)
+						else:
+							if int(wrange[0]) <= int(band+startDate-1) <= int(wrange[1]):
+								srcband = ds.GetRasterBand(band)
+								structval = srcband.ReadRaster(int(px), int(py), 1, 1, buf_type=srcband.DataType )
+								bandtype = gdal.GetDataTypeName(srcband.DataType)
+								intval = struct.unpack(fmttypes[bandtype] , structval)
+								if wavg:
+									avg += (round((float(intval[0])/100),2))+factor
+								else:
+									output_item = {'date' : int(band+startDate-1) , 'value' : ((round((float(intval[0])/100),2))+factor)-baselineAvg}
+									json_output['values'].append(output_item)
+					if avg != 0 and wavg:
+						avg = (avg / (int(wrange[1]) - int(wrange[0]) + 1)) - baselineAvg +factor
+						output_item = {'date' : 'avg_'+wrange[0]+"-"+wrange[1] , 'value' : str(avg)}
+						json_output['values'].append(output_item)
+					elif avg == 0 and wavg:
+						output_item = {'date' : 'avg_'+wrange[0]+"-"+wrange[1] , 'value' : 'out of range'}
+						json_output['values'].append(output_item)
+					# return json_output
+
+				else:
+					for band in range( bands ):
+						band += 1
 						srcband = ds.GetRasterBand(band)
 						structval = srcband.ReadRaster(int(px), int(py), 1, 1, buf_type=srcband.DataType )
 						bandtype = gdal.GetDataTypeName(srcband.DataType)
 						intval = struct.unpack(fmttypes[bandtype] , structval)
-						if wavg:
-							avg += (round((float(intval[0])/100),2))+factor
-						else:
+						if wclm:
+							output_item = {'date' : str(int(monthlyYear+startDate))+"-"+str(int(((band-1)%12)+1)) , 'value' : ((round((float(intval[0])/100),2))+factor)-baselineAvg}
+							if (band % 12) == 0:
+								monthlyYear += 1
+						else: 
 							output_item = {'date' : int(band+startDate-1) , 'value' : ((round((float(intval[0])/100),2))+factor)-baselineAvg}
-							json_output['values'].append(output_item)
-				if avg != 0 and wavg:
-					avg = (avg / (int(wrange[1]) - int(wrange[0]) + 1)) - baselineAvg +factor
-					output_item = {'date' : 'avg_'+wrange[0]+"-"+wrange[1] , 'value' : str(avg)}
-					json_output['values'].append(output_item)
-				elif avg == 0 and wavg:
-					output_item = {'date' : 'avg_'+wrange[0]+"-"+wrange[1] , 'value' : 'out of range'}
-					json_output['values'].append(output_item)
-				return json_output
-
-			else:
-				for band in range( bands ):
-					band += 1
-					srcband = ds.GetRasterBand(band)
-					structval = srcband.ReadRaster(int(px), int(py), 1, 1, buf_type=srcband.DataType )
-					bandtype = gdal.GetDataTypeName(srcband.DataType)
-					intval = struct.unpack(fmttypes[bandtype] , structval)
-					output_item = {'date' : int(band+startDate-1) , 'value' : ((round((float(intval[0])/100),2))+factor)-baselineAvg}
-					json_output['values'].append(output_item)
-				return json_output
+						json_output['values'].append(output_item)
+					# return json_output
+		return json_output	
 	else :
 		return {"error":"Data not found"}
